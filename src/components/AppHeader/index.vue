22e7363b0839d2544dfea6e35fbb649ca99d0412
Andressa Paranhos <andressaparanhos1@hotmail.com>
2023-07-06 23:46:42 -0300

    feature(AppHeader): Added AppHeader component

---
+++ src/components/AppHeader/index.vue
@@ -0,0 +1,55 @@
+<template>
+    <header>
+        <div class="container">
+            <img class="logo" src="../../assets//logo-rachi.png" alt="Rachi">
+            <nav class="navbar">
+                <div class="desktop-menu">
+                    <ul class="items-menu">
+                        <li v-for="item in menuItems" :key="item.id">
+                            <a :href="item.url">
+                                {{ item.name }}
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="mobile-menu">
+                    <button class="navbar-toggler" @click="toggleMenu">
+                        <img src="@/assets/icons/menu-icon.svg" alt="Menu">
+                    </button>
+                    <div class="navbar-collapse" :class="{'show': isMenuOpen}">
+                        <ul v-if="isMenuOpen">
+                            <li v-for="item in menuItems" :key="item.id">
+                                <a :href="item.url">
+                                    {{ item.name }}
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    </header>
+</template>
+
+<script>
+    export default {
+        name: 'AppHeader',
+        data () {
+            return {
+                menuItems: [
+                    { id: 1, name: 'Funcionalidades', url: '#'},
+                    { id: 2, name: 'App', url: '#'},
+                    { id: 3, name: 'Planos', url: '#'},
+                    { id: 4, name: 'Contatos', url: '#'}
+                ],
+                isMenuOpen: false
+            }
+        },
+        methods: {
+            toggleMenu () {
+                this.isMenuOpen = !this.isMenuOpen
+            }
+        }
+    }
+</script>
+<style src="./styles.scss" scoped lang="scss"></style>
