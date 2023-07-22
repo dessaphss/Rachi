3912bbfc5b059f9b1a349c8e93990e1859578cca
Andressa Paranhos <andressaparanhos1@hotmail.com>
2023-07-22 04:08:39 -0300

    feature: created max and max-full container components

---
+++ src/components/ContainerItem/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div :class="containerClass">
+    <slot></slot>
+  </div>
+</template>
+<script>
+export default {
+  name: 'ContainerItem',
+  props: {
+    containerClass: {
+      type: String,
+      default: 'max'
+    }
+  }
+}
+</script>
+<style scoped lang="scss">
+  .max {
+    max-width: 1200px;
+    margin: 0 auto;
+    padding: 0 1.2rem 0 1.2rem;
+    &.max-full {
+      max-width: 100%;
+      padding: 0 1.2rem 0 1.2rem;
+    }
+  }
+</style>
